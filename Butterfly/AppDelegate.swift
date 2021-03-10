e3bf2a77d5909a580374e7e53e397191ff5921f7
hiroaki Iwai <hiro_lily_da@hotmail.com>
2021-03-10 22:57:56 +0900

    ステータスバーアプリの設定と仮アイコンの設定

--- Butterfly/AppDelegate.swift
+++ Butterfly/AppDelegate.swift
@@ -9,18 +9,21 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    
-
+    private let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named:NSImage.Name("icon"))
+            button.imagePosition = .imageLeft
+            button.action = #selector(show(_:))
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
-
+    @objc func show(_ sender: Any?) {
+    }
 }
 
