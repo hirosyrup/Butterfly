4a8d661e95cf8c484a4520f5d04d26dd7ec86fab
Hiroaki Iwai <h-iwai@kiizan-kiizan.co.jp>
2021-03-10 14:12:31 +0900

    新規プロジェクト追加

---
+++ Butterfly/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Butterfly
+//
+//  Created by 岩井宏晃 on 2021/03/10.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
