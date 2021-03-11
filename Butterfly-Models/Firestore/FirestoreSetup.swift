104f14655be7cf293fc30e174cbe2e0bac5e4296
hiroaki Iwai <hiro_lily_da@hotmail.com>
2021-03-11 22:53:51 +0900

    iOS用のターゲットの追加と共通モデル用のフォルダ分け

---
+++ Butterfly-Models/Firestore/FirestoreSetup.swift
@@ -0,0 +1,15 @@
+//
+//  FirestoreSetup.swift
+//  Butterfly
+//
+//  Created by 岩井 宏晃 on 2021/03/11.
+//
+
+import Foundation
+import Firebase
+
+class FirestoreSetup {
+    func setup() {
+        FirebaseApp.configure()
+    }
+}
--- Podfile
+++ Podfile
@@ -5,3 +5,11 @@ pod 'FirebaseAuth'
 pod 'Firebase/Firestore'
 pod 'FirebaseFirestoreSwift'
 end
+
+target 'Butterfly-iOS' do
+use_frameworks!
+
+pod 'FirebaseAuth'
+pod 'Firebase/Firestore'
+pod 'FirebaseFirestoreSwift'
+end
